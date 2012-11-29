0bb8a8969e5b1abe60f0c3b69361bab3688ef601
Shaobo Hou <shaobohou@gmail.com>
2012-11-29 18:40:39 +0000

    Better idiot player.

--- java/fourplayclient/src/main/java/net/swiftkey/fourplay/IdiotPlayer.java
+++ java/fourplayclient/src/main/java/net/swiftkey/fourplay/IdiotPlayer.java
@@ -4,11 +4,16 @@ import java.util.Random;
 
 public class IdiotPlayer implements Player {
 
-	Random mRandom = new Random();
+    Random mRandom = new Random();
 	
-	@Override
-	public int move(Board b) {
-		return mRandom.nextInt(b.countCols());
-	}
+    @Override
+    public int move(Board b) {
+        int move = mRandom.nextInt(b.countCols());
+        while(b.nextRow(move)<0) {
+            move = mRandom.nextInt(b.countCols());
+        }
+
+        return move;
+    }
 
 }
