467c75c56307368f2934e8b38b61d02d82bfcae3
Shaobo Hou <shaobohou@gmail.com>
2012-12-02 19:54:19 +0000

    A simple minimax player.

---
+++ java/fourplayclient/src/main/java/net/swiftkey/fourplay/MinimaxPlayer.java
@@ -0,0 +1,96 @@
+package net.swiftkey.fourplay;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MinimaxPlayer implements Player {
+
+    Player mIdiot = new IdiotPlayer();
+    Random mRandom = new Random();
+    int mDepth = 3;
+
+    public MinimaxPlayer(int depth) {
+        mDepth = depth;
+    }
+	
+    @Override
+    public int move(Board b) {
+        double startTime = System.currentTimeMillis();
+
+        int[] scores = new int[b.countCols()];
+
+        try {
+            // find all valid moves
+            int best = -1;
+            for (int col = 0; col < b.countCols(); ++col) {
+                scores[col] = -999;
+                if (b.nextRow(col)>=0) {
+                    // take the winning move
+                    if(b.willWin(col, 4, 1)) {
+                        System.out.println("Found a winning move at column " + col);
+                        return col;
+                    }
+
+                    // minimax search
+                    scores[col] = -search(b.withMove(col).invert(), mDepth*2-1);
+                    if (best<0 || scores[col]>scores[best]) {
+                        best = col;
+                    }
+                }
+            }
+
+            // System.out.println(Arrays.toString(scores));
+            // double finishTime = System.currentTimeMillis();
+            // System.out.println("took " + (finishTime-startTime));
+
+            ArrayList<Integer> moves = new ArrayList<Integer>();
+            if (best>=0) {
+                for (int col = 0; col < b.countCols(); ++col) {
+                    if (scores[col]==scores[best]) {
+                        moves.add(col);
+                    }
+                }
+
+                return moves.get(mRandom.nextInt(moves.size()));
+            }
+        } catch (Exception e) {
+            System.out.println("Exception!");
+        }
+
+        return mIdiot.move(b);
+    }
+
+
+    int search(Board b, int depth) throws Exception {
+        if(depth<0) {
+            return 0;
+        }
+
+        for (int col = 0; col < b.countCols(); ++col) {
+            if (b.nextRow(col)>=0) {
+                if (b.willWin(col, 4, 1)) {
+                    return 100;
+                }
+            }
+        }
+
+        int best = -1;
+        int[] scores = new int[b.countCols()];
+        for (int col = 0; col < b.countCols(); ++col) {
+            scores[col] = -999;
+            if (b.nextRow(col)>0) {
+                scores[col] = -search(b.withMove(col).invert(), depth-1);
+                if (best<0 || scores[col]>scores[best]) {
+                    best = col;
+                }
+            }
+        }
+
+        if (best>=0) {
+            return scores[best];
+        }
+
+        return 0;
+    }
+}
